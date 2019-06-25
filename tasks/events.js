8d6c92377a7d9a0ed3f5e2fdfcc1763839c0278b
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-25 16:26:12 +0800

    adding all the events

--- tasks/events.js
+++ tasks/events.js
@@ -5,8 +5,15 @@ module.exports = (client) => {
 	client.on('message', (message) => { Event('message')(client, message); });
 	client.on('guildCreate', (guild) => { Event('guildCreate')(client, guild); });
 	client.on('guildDelete', (guild) => { Event('guildDelete')(client, guild); });
+	// client.on('guildUpdate', (oldGuild, newGuild) => { Event('guildUpdate')(client, oldGuild, newGuild); });
 	// client.on('guildMemberAdd', (member) => { Event('guildMemberAdd')(client, member); });
 	// client.on('guildMemberRemove', (member) => { Event('guildMemberRemove')(client, member); });
+	// client.on('guildMemberUpdate', (oldMember, newMember) => { Event('guildMemberUpdate')(client, oldMember, newMember); });
+	// client.on('guildMemberAdd', (role) => { Event('guildMemberAdd')(client, role); });
+	// client.on('guildMemberRemove', (role) => { Event('guildMemberRemove')(client, role); });
+	// client.on('guildMemberUpdate', (oldRole, newRole) => { Event('guildMemberUpdate')(client, oldRole, newRole); });
+	// client.on('guildBanAdd', (guild, user) => { Event('guildMemberAdd')(client, guild, user); });
+	// client.on('guildBanRemove', (guild, user) => { Event('guildMemberRemove')(client, guild, user); });
 	// client.on('messageReactionAdd', (reaction, user) => { Event('messageReactionAdd')(client, reaction, user); });
 	// client.on('messageReactionRemove', (reaction, user) => { Event('messageReactionRemove')(client, reaction, user); });
 };
---
+++ templates/event.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2019  Joshua McCrystal & Dimitri Lambrou
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Written by Joshua McCrystal <joshua.mccrystal@hotmail.com>, 25/06/2019
+*/
+
+module.exports = async (client) => {
+	const test = client.user;
+	console.log(test);
+};
