f592272209597a55217edb5a49a578c8631033c1
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-25 11:56:31 +0800

    forgot it aint needed, <3 open source

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) 2019  Joshua McCrystal & Dimitri Lambrou
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Written by Joshua McCrystal <joshua.mccrystal@hotmail.com>, 25/06/2019
-*/
-
 module.exports = async (client) => {
 	client.user.setPresence({ game: { name: 'with the restart switch!' }, status: 'dnd' });
 	console.log(`INFO) ${client.user.tag} is ready to serve ${client.guilds.size} guilds and ${client.users.size} users!`);
