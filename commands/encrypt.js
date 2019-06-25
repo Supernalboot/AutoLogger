a46cd49d418beb3e56d2a9cb4db6527e8470d93c
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-25 15:43:47 +0800

    fixed decrypt in case of false decryption

--- commands/encrypt.js
+++ commands/encrypt.js
@@ -21,43 +21,46 @@ module.exports = {
 		// Delete command message if given permission
 		if (message.deletable) message.delete();
 
+		// Variables
+		let msg; let code
+
 		// Send messaging asking for message to encrypt
 		await message.author.send('**Ready to encrypt!** What message would you like to encrypt?\n*Just type it out and send.*').then(async (dm) => {
 			// Check if already in DMs
 			if (message.channel.type === 'dm') return;
 			message.reply(' I\'ve sent you a DM!');
 			// Await a reply
-			msgs = await dm.channel.awaitMessages(reply => reply.author.id === message.author.id, { max: 1, time: 60000 });
+			const msgs = await dm.channel.awaitMessages(reply => reply.author.id === message.author.id, { max: 1, time: 60000 });
 			// Save msg in a variable
-			const msg = msgs.first().content;
+			msg = msgs.first().content;
 			// Delete original messages
-			dm.delete();
+			await dm.delete();
 
 			// Send messaging asking for the key to encryption
 			const end = await message.author.send('**Got it!** What is the key to this message?\n*Key can be any ASCII characters up to 32 long. e.g. 143fg34h5g2 or secret50key*');
 			// Await a reply
-			codes = await dm.channel.awaitMessages(reply => reply.author.id === message.author.id, { max: 1, time: 60000 });
+			const codes = await dm.channel.awaitMessages(reply => reply.author.id === message.author.id, { max: 1, time: 60000 });
 			// Save msg in a variable
-			const code = codes.first().content;
+			code = codes.first().content;
 			// Delete original message
-			end.delete();
-
-			// Encrypt our message
-			const encryption = CryptoJS.AES.encrypt(msg, code);
-
-			// Create our discord embed
-			const embed = new Discord.RichEmbed()
-				.setTitle('Message Encryption')
-				.addField('Encrypted Message', encryption)
-				.setFooter(`Key: ${code}`)
-				.setColor(client.color.basic('yellow'));
-
-			// Send our Encrypted message
-			return message.author.send(embed);
+			await end.delete();
 		}).catch(() => {
 			// If DMs fail let them know
 			return message.reply(' it seems like I can\'t DM you! Do you have DMs disabled?');
 		});
+
+		// Encrypt our message
+		const encryption = CryptoJS.AES.encrypt(msg, code).catch(() => { message.author.send('There was a problem decrypting your message, please check the encryption and key and try again.') });
+
+		// Create our discord embed
+		const embed = new Discord.RichEmbed()
+			.setTitle('Message Encryption')
+			.addField('Encrypted Message', encryption)
+			.setFooter(`Key: ${code}`)
+			.setColor(client.color.basic('yellow'));
+
+		// Send our Encrypted message
+		return message.author.send(embed);
 
 	},
 };
