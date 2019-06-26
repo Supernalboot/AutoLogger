06c8724e6aad72fe879db2b14232084d8cfead20
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-26 12:00:23 +0800

    creating role create logger

--- events/roleCreate.js
+++ events/roleCreate.js
@@ -7,7 +7,7 @@ module.exports = async (client, role) => {
 
 	// Fetch latest audit, to make sure we will fetch this specific task
 	const audit = await guild.fetchAuditLogs({ limit: 1, type: 30 });
-	const entry = audit.entries.first();
+	const entry = await audit.entries.first();
 
 	await client.channels.get('592845625209389069').send(entry.executor);
 
