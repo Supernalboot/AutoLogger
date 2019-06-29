ace9f2cdf4986bb750e1f7a66a3d97faf8dd72ef
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-29 13:22:57 +0800

    added guild join message

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,8 +2,9 @@ const Discord = require('discord.js');
 
 module.exports = async (client, guild) => {
 	guild.fetchMembers('', guild.members.size);
+
 	// Add information to table
-	client.knex.from('guilddata').where('guildid', guild.id).then(async function(output) {
+	await client.knex.from('guilddata').where('guildid', guild.id).then(async function(output) {
 		if (!output[0]) {
 			// Make database changes
 			client.knex('guilddata')
@@ -14,6 +15,18 @@ module.exports = async (client, guild) => {
 			client.knex.from('guilddata').where('guildid', guild.id).update('isactive', true).then(console.log(`[DATA] Guild ${guild.id} rejoined, isActive is now TRUE!`));
 		}
 	});
+
+	// Prefix
+	let prefix;
+	await client.knex.from('guilddata').where('guildid', guild.id).select('prefix').then(async function(output) { prefix = await output[0].prefix; });
+
+	// DM server owner with information
+	await guild.owner.send(`Thank you (or a member with admin) for inviting me to **${guild.name}**\n
+I am ${client.user}, and I am here to hopefully make your moderation life in this server easier!
+I can log just about anything and my logging is highly customizable! Do \`${prefix}setlogs all [Channel ID] to start logging.
+I can also peform most admin commands like, mute, kick and ban etc. (COMING SOON)\`
+Something unique about me is I can \`${prefix}encrypt\` and \`${prefix}decrypt\` messages for you.`);
+
 	// Guild join embed
 	const bots = guild.members.filter(m => m.user.bot).size;
 	const humans = guild.memberCount - bots;
