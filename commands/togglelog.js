fe5eaa765311b980393d739e4a9ce9b87a6f781d
TyrdleCode <44824998+TyrdleCode@users.noreply.github.com>
2019-06-29 13:02:22 +0800

    i forgot what I changed lol

--- commands/togglelog.js
+++ commands/togglelog.js
@@ -3,7 +3,7 @@ module.exports = {
 	info: 'Toggle log modules on or off',
 	desc: 'Decide which logs you want on or off, can do almost every event',
 	aliases: ['toggle'],
-	usage: '(module) (on/off)',
+	usage: '(module)',
 	args: false,
 	guildOnly: true,
 	ownerOnly: false,
@@ -14,6 +14,7 @@ module.exports = {
 
 	/** - - Code to Run - - */
 	async execute(client, message, args) {
+		if (message.deletable) message.delete();
 
 		// If no args given, show what modules are currently set too.
 		if (!args.length) {
@@ -30,15 +31,31 @@ module.exports = {
 				.addField('ChannelCreate', `${data.channelcreate.toString().replace('true', '✅').replace('false', '🅾')}`, true)
 				.addField('ChannelDelete', `${data.channeldelete.toString().replace('true', '✅').replace('false', '🅾')}`, true)
 				.addField('ChannelUpdate', `${data.channelupdate.toString().replace('true', '✅').replace('false', '🅾')}`, true)
-				.addField('RoleCreate', `${data.messagecreate.toString().replace('true', '✅').replace('false', '🅾')}`, true)
-				.addField('RoleDelete', `${data.messagedelete.toString().replace('true', '✅').replace('false', '🅾')}`, true)
-				.addField('RoleUpdate', `${data.messagedelete.toString().replace('true', '✅').replace('false', '🅾')}`, true)
+				.addField('RoleCreate', `${data.rolecreate.toString().replace('true', '✅').replace('false', '🅾')}`, true)
+				.addField('RoleDelete', `${data.roledelete.toString().replace('true', '✅').replace('false', '🅾')}`, true)
+				.addField('RoleUpdate', `${data.roleupdate.toString().replace('true', '✅').replace('false', '🅾')}`, true)
 				.addField('UserPFP', `${data.userpfp.toString().replace('true', '✅').replace('false', '🅾')}`, true)
 				.addField('Username', `${data.username.toString().replace('true', '✅').replace('false', '🅾')}`, true)
 				.setColor(client.color.basic('blue'));
 			// Send embed
-			return logChannel.send(embed);
+			return message.channel.send(embed);
+
 		} else {
+
+			// See if module exists
+			let mod;
+			await client.knex.from('guilddata').where('guildid', message.guild.id).select(args[0].toLowerCase()).then(async function(output) {
+				if (output[0]) mod = await output[0][args[0].toLowerCase()];
+			}).catch(error => { message.channel.send(`Sorry, ${args[0]} is not a module.`); throw error; });
+
+			// Update/toggle module
+			if (mod) {
+				await client.knex.from('guilddata').where('guildid', message.guild.id).update(args[0].toLowerCase(), 'false');
+				return message.channel.send(`\`${args[0]}\` logs have been **disabled**.`);
+			} else {
+				await client.knex.from('guilddata').where('guildid', message.guild.id).update(args[0].toLowerCase(), 'true');
+				return message.channel.send(`\`${args[0]}\` logs have been **enabled**.`);
+			}
 
 		}
 
